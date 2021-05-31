349fa20a279c6edb8dc8020c68f733cb7b38f1f5
Stefanos Michalas <sm@think-plus.gr>
2021-05-31 17:53:04 +0300

    played with posts and users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
 
 class UserController extends Controller
 {
@@ -13,5 +16,14 @@ class UserController extends Controller
             'email' => $user->email,
             'username' => $user->username
         ]);
+    }
+
+    // get all comments left on the user's posts!
+    public function comments ($id) {
+        $user = User::find($id);
+        $comments = Comment::whereHas('post', function ($query) {
+            return $query->where('user_id', 1);
+        })->get();
+        return response()->json($comments);
     }
 }
